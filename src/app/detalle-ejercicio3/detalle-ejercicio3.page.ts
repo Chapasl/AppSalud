396fc6729a316e7e11e09201e8c9cace32828515
Chapasl <42504676+Chapasl@users.noreply.github.com>
2021-06-09 00:24:25 -0500

    correciones

--- src/app/detalle-ejercicio3/detalle-ejercicio3.page.ts
+++ src/app/detalle-ejercicio3/detalle-ejercicio3.page.ts
@@ -12,9 +12,7 @@ export class DetalleEjercicio3Page implements OnInit {
     slidesPerView:2.6
   };
 // eslint-disable-next-line @typescript-eslint/naming-convention,max-len
-  categoriesArray = [{nombre: 'Lugar abierto', img:'Ingrediente_1_detalle_ejercicios_3.jpg'} ,
-    {nombre: 'Ejercicios', img:'sección_ejercicio.jpg'} ,
-    {nombre: 'Belleza', img:'sección_belleza.jpg'}];
+  categoriesArray = [{nombre: 'Lugar abierto', img:'Ingrediente_1_detalle_ejercicios_3.jpg'} ];
   constructor() { }
 
   ngOnInit() {
