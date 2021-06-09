ceb504637977c3a74cf97e3febbde2449d543159
Chapasl <42504676+Chapasl@users.noreply.github.com>
2021-06-08 19:37:35 -0500

    Cat Comida

--- src/app/category-comida/category-comida.page.ts
+++ src/app/category-comida/category-comida.page.ts
@@ -8,15 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class CategoryComidaPage implements OnInit {
 
   // eslint-disable-next-line max-len
-  comidaArray = [{nombre: 'Escalador (mountain climbers)', img:'category_comida_1.jpg', description:'El escalador es un excelente ejercicio para fortalecer el abdomen, brazos y hombros. Es sencillo y sus resultados son magníficos.', tiempo: '10 min', grado: 'Fácil', linkpage: '/detalle-comida'} ,
+  comidaArray = [{nombre: 'Pastel sin azúcar', img:'category_comida_1.jpg', description:'Delicioso pastel con nueces, sencillo de hacer y lo más importante, sin azúcar, para cuidar la salud de tu familia.', tiempo: '60 min', grado: 'Fácil', linkpage: '/detalle-comida'} ,
     // eslint-disable-next-line max-len
-    {nombre: 'Mascarrillas caseras', img:'category_comida_2.jpg', description:'Si tienes la piel grasa, una mascarilla sencilla de hacer y muy efectiva es la siguiente.', tiempo: '5 min', grado: 'Fácil', linkpage: '/detalle-comida2'} ,
+    {nombre: 'Sanduche con mayonesa saludable', img:'category_comida_2.jpg', description:'Evita el uso de mayonesa y productos grasos con esta sabrosa alternativa.', tiempo: '5 min', grado: 'Fácil', linkpage: '/detalle-comida2'} ,
     // eslint-disable-next-line max-len
-    {nombre: 'Postres naturales', img:'category_comida_3.jpg', description:'La receta de hoy les va a encantar, no puede ser más fácil y tampoco estar más rica.', tiempo: '30 min', grado: 'Medio', linkpage: '/detalle-comida3'} ,
+    {nombre: 'Brochetas de res', img:'category_comida_3.jpg', description:'Invita a tus familiares un delicioso aperitivo, es nutritivo y rápido.', tiempo: '30 min', grado: 'Medio', linkpage: '/detalle-comida3'} ,
     // eslint-disable-next-line max-len
-    {nombre: 'Mascarrillas caseras', img:'category_comida_4.jpg', description:'Si tienes la piel grasa, una mascarilla sencilla de hacer y muy efectiva es la siguiente.', tiempo: '5 min', grado: 'Fácil', linkpage: '/detalle-comida'} ,
+    {nombre: 'Sushi casero', img:'category_comida_4.jpg', description:'Elabora un delicioso sushi casero, sin materiales complicados de conseguir.', tiempo: '20 min', grado: 'Fácil', linkpage: '/detalle-comida'} ,
     // eslint-disable-next-line max-len
-    {nombre: 'Mascarrillas caseras', img:'category_comida_5.jpg', description:'Si tienes la piel grasa, una mascarilla sencilla de hacer y muy efectiva es la siguiente.', tiempo: '5 min', grado: 'Fácil', linkpage: '/detalle-comida'} ];
+    {nombre: 'Lomo de cerdo', img:'category_comida_5.jpg', description:'Lomo de cerdo acompañado de papas fritas, perfectas para un fin de semana en familia.', tiempo: '55 min', grado: 'Difícil', linkpage: '/detalle-comida'} ];
   constructor() { }
 
   ngOnInit() {
