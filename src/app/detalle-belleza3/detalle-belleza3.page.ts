1944da6008a14f0ff2d317512a7b87a0ccec786d
Chapasl <42504676+Chapasl@users.noreply.github.com>
2021-06-08 23:26:40 -0500

    detalle 3 belleza

--- src/app/detalle-belleza3/detalle-belleza3.page.ts
+++ src/app/detalle-belleza3/detalle-belleza3.page.ts
@@ -12,9 +12,9 @@ export class DetalleBelleza3Page implements OnInit {
     slidesPerView:2.6
   };
 // eslint-disable-next-line @typescript-eslint/naming-convention,max-len
-  categoriesArray = [{nombre: 'Comidas', img:'sección_comida.jpg', linkpage: 'category-comida'} ,
-    {nombre: 'Ejercicios', img:'sección_ejercicio.jpg', linkpage: 'category-ejercicio'} ,
-    {nombre: 'Belleza', img:'sección_belleza.jpg', linkpage: 'category-belleza'}];
+  categoriesArray = [{nombre: 'Lugar tranquilo', img:'Ingrediente_3_detalle_belleza_3'} ,
+    {nombre: 'Velas aromáticas', img:'Ingrediente_2_detalle_belleza_3'} ,
+    {nombre: 'Asiento comodo', img:'Ingrediente_1_detalle_belleza_3'}];
   constructor() { }
 
   ngOnInit() {
