eb1190a428317c1349551f5e6c6bbdc35359267f
Johny-Bautista <jsbautista@uce.edu.ec>
2021-06-10 10:51:20 -0500

    cambio
    botón y class simetry

--- src/app/contacto/contacto.page.ts
+++ src/app/contacto/contacto.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators} from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
 
+
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.page.html',
